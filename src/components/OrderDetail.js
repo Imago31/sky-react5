e27899a65ed758470fb0d3b50c562604eb8cefc2
Imago31 <xaker201301@gmail.com>
2022-05-12 15:53:39 +0200

    lesson_5

--- src/components/OrderDetail.js
+++ src/components/OrderDetail.js
@@ -1,8 +1,19 @@
+/* eslint-disable react/button-has-type */
+
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/self-closing-comp */
+
+
 
 export default function OrderDetail(props) {
   return (
     <div className="order-detail">
-     
+        <h4 className="product-name">{props.productName}</h4>
+        <p className="productName">Price: {props.price} /-</p>
+        <p className="productName">Quantity: {props.quantity}</p>
+        <button onClick={props.handlePlus} className="add-button">+</button>
+
     </div>
   )
 }
